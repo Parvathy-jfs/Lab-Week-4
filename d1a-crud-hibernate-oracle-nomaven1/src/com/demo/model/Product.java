c3b33bc96f38b2cc4058cdcc856352872e546d2c
Parvathy-jfs <59194787+Parvathy-jfs@users.noreply.github.com>
2020-01-17 08:59:49 +0500

    Add files via upload

---
+++ d1a-crud-hibernate-oracle-nomaven1/src/com/demo/model/Product.java
@@ -0,0 +1,29 @@
+package com.demo.model;
+
+public class Product {
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + "]";
+	}
+	public Product(String productId, String productName) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+	}
+	String productId;
+	String productName;
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+
+}
